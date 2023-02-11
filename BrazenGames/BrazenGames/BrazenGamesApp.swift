fb63b278cc7d724d71f12d42164cd8bd7ca26fc7
John Khester <designe.creative@mail.ru>
2023-02-12 02:39:25 +0300

    Add files via upload

---
+++ BrazenGames/BrazenGames/BrazenGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrazenGamesApp.swift
+//  BrazenGames
+//
+//  Created by Andrey on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct BrazenGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
